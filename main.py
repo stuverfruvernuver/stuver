11e66374f3b879411921ca62e560aef215456f58
stuverfruvernuver <johndyestuve@gmail.com>
2025-04-13 17:37:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,8 @@
 import asyncio
-import json
-import time
 from chat import connect_to_chat
-from discord_notifier import send_discord_ping, periodic_discord_pings
+from discord_notifier import send_discord_ping
 from stream_checker import periodic_stream_checks
-
-with open("config.json") as f:
-    config = json.load(f)
-
-CHANNEL_NAME = config["channel_name"]
 
 if __name__ == "__main__":
     send_discord_ping()
-    asyncio.run(connect_to_chat(CHANNEL_NAME))
+    asyncio.run(connect_to_chat())
